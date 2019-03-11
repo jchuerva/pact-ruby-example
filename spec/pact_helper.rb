013d4ffaa7ff11d21b6ff8d016bf62d77ff2135d
Carlos Alarcon <jchuerva@github.com>
2019-03-11 19:17:33 +0100

    verify pact against producer

--- spec/pact_helper.rb
+++ spec/pact_helper.rb
@@ -9,3 +9,14 @@ Pact.service_consumer "Our Consumer" do
     end
   end
 end
+
+# Provider
+require 'pact/provider/rspec'
+
+Pact.service_provider "Our Provider" do
+
+  honours_pact_with 'Our Consumer' do
+    pact_uri 'spec/pacts/our_consumer-our_provider.json'
+  end
+
+end
