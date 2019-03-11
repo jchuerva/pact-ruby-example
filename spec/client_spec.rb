a7f85dd7713a3d94121e7c5058e6e1842eea2be2
Carlos Alarcon <jchuerva@github.com>
2019-03-11 19:47:45 +0100

    simplify example

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,14 +6,13 @@ describe Client do
   let(:json_data) do
     {
       "test" => "NO",
-      "valid_date" => "2013-08-16T15:31:20+10:00",
-      "count" => 100
+      "valid_date" => "2013-08-16T15:31:20+10:00"
     }
   end
   let(:response) { double('Response', :success? => true, :body => json_data.to_json) }
 
   it 'can process the json payload from the provider' do
     allow(HTTParty).to receive(:get).and_return(response)
-    expect(subject.process_data(Time.now.httpdate)).to eql([1, Time.parse(json_data['valid_date'])])
+    expect(subject.process_data(Time.now.httpdate)).to eql(Time.parse(json_data['valid_date']))
   end
 end
