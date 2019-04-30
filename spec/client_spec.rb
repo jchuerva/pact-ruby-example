3056a01bfcbd709a7e7d9661a65225ee197e92be
Carlos Alarcon <jchuerva@github.com>
2019-04-30 13:06:53 +0200

    comment client test as it's replaced by pact test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -9,12 +9,12 @@ describe Client do
       "valid_date" => "2013-08-16T15:31:20+10:00"
     }
   end
-  let(:response) { double('Response', :success? => true, :body => json_data.to_json) }
+  # let(:response) { double('Response', :success? => true, :body => json_data.to_json) }
 
-  it 'can process the json payload from the provider' do
-    allow(HTTParty).to receive(:get).and_return(response)
-    expect(subject.process_data(Time.now.httpdate)).to eql Time.parse(json_data['valid_date'])
-  end
+  # it 'can process the json payload from the provider' do
+  #   allow(HTTParty).to receive(:get).and_return(response)
+  #   expect(subject.process_data(Time.now.httpdate)).to eql Time.parse(json_data['valid_date'])
+  # end
 
   describe 'Pact with our provider', :pact => true do
 
